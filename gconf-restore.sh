aa8df29ae12c8f9d5d4f53cd8c4f6747a616bb21
René Six <mail@rene-six.de>
2012-06-03 15:52:01 +0200

    copy gtk-bookmarks back to place and unset everything with gconftool

--- gconf-restore.sh
+++ gconf-restore.sh
@@ -3,6 +3,11 @@
 
 DUMP=gconf
 
+gconftool-2 --recursive-unset /
+[ -d ~/.gconf ] && rm -rf ~/.gconf
+
 for f in $(find ${DUMP} -type f -name '*.xml'); do
     gconftool-2 --load=${f}
 done
+
+cp gtk-bookmarks ~/.gtk-bookmarks
