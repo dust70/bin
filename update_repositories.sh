2b013e5e3d266818b9b0a12b873f0c29ca906c54
René Six <mail@rene-six.de>
2013-07-29 13:06:10 +0200

    update file update_repositories.sh in repository on date 2013-07-29
    Signed-off-by: René Six <mail@rene-six.de>

--- update_repositories.sh
+++ update_repositories.sh
@@ -131,6 +131,8 @@ git_repos["php/contao"]="git://github.com/contao/core.git"
 git_repos["php/contao-check"]="git://github.com/contao/check.git"
 git_repos["php/contao-docs"]="git://github.com/contao/docs.git"
 
+git_repos["php/phpmyadmin"]="git://github.com/phpmyadmin/phpmyadmin.git"
+
 git_repos["php/shopware"]="git://github.com/ShopwareAG/shopware-4.git"
 
 git_repos["php/tcpdf"]="git://git.code.sf.net/p/tcpdf/code"
