357935f4c01cb70c58c63b0decd80cd16aebc3af
René Six <info@renatius.de>
2014-05-29 19:33:11 +0200

    add file load_keychain.sh to repository

---
+++ load_keychain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#set -x
+
+if ! $(which keychain > /dev/null 2>&1); then
+    echo "please install keychain"
+    exit 1
+fi
+
+declare -a ssh_keys_pub=( ~/.ssh/*.pub )
+declare -a ssh_keys
+
+j=0
+for i in "${ssh_keys_pub[@]}"; do
+    ssh_keys[$j]=$(basename ${i%%.pub})
+    ((j++))
+done
+
+keychain "${ssh_keys[@]}" F91E87BC
+
+# vim: foldmethod=marker textwidth=80 filetype=sh
