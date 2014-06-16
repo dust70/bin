6d553d59d27873a2703a8bbf447216bbe489f2a5
René Six <info@renatius.de>
2014-06-16 10:21:14 +0200

    host amalthea don't exists anymore

--- update_repositories.sh
+++ update_repositories.sh
@@ -83,7 +83,7 @@ branches["dotfiles/dotmail"]="desktop"
 branches["dotfiles/local"]="desktop"
 branches["dotfiles/ssh"]="desktop"
 
-branches["etc"]="amalthea desktop europa ganymed himalia io server"
+branches["etc"]="desktop europa ganymed himalia io server"
 
 branches["php/zend_framework"]="develop"
 #}}}
