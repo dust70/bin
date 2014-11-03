7f76ee41657619929864623a4f4f223c8cfadf6b
René Six <info@renatius.de>
2014-11-03 11:49:50 +0100

    update file cscope-gen.sh in repository on date 2014-11-03

--- cscope-gen.sh
+++ cscope-gen.sh
@@ -2,15 +2,17 @@
 for arg in "${@}"; do
     if [ -d ${arg}  ]; then
         find ${arg} \
-            -iname "*.c" \
-            -or -iname "*.cc" \
-            -or -iname "*.cpp" \
-            -or -iname "*.h" \
-            -or -iname "*.hpp"  \
-            -or -iname "*.java" \
-            -or -iname "*.php" \
-            -or -iname "*.py" \
-            -or -iname "*.tpl" \
+            -type f \( \
+                -iname "*.c" \
+                -or -iname "*.cc" \
+                -or -iname "*.cpp" \
+                -or -iname "*.h" \
+                -or -iname "*.hpp"  \
+                -or -iname "*.java" \
+                -or -iname "*.php" \
+                -or -iname "*.py" \
+                -or -iname "*.tpl" \
+            \) \
             > ${arg}/cscope.files
 
         cscope -b -q -i ${arg}/cscope.files -f ${arg}/cscope.out
